bf00e46de10704215d9693f51ef42302969a0a8d
dsafdsaf132 <dsafdsaf132@gmail.com>
2026-05-18 14:07:56 +0900

    Use weakest scale for arc minimum width

--- wasm/src/renderer/shader.rs
+++ wasm/src/renderer/shader.rs
@@ -198,10 +198,22 @@ out highp float vRadius;
 out highp float vStartAngle;
 out highp float vSweepAngle;
 out highp float vThickness;
+
+float weakestPixelsPerWorld() {
+    vec2 pixelScale = viewport_size * 0.5;
+    vec2 axisX = (mat2(transform) * vec2(1.0, 0.0)) * pixelScale;
+    vec2 axisY = (mat2(transform) * vec2(0.0, 1.0)) * pixelScale;
+    float a = dot(axisX, axisX);
+    float b = dot(axisX, axisY);
+    float d = dot(axisY, axisY);
+    float trace = a + d;
+    float discriminant = sqrt(max((a - d) * (a - d) + 4.0 * b * b, 0.0));
+    float weakestScaleSquared = max((trace - discriminant) * 0.5, 0.0);
+    return sqrt(weakestScaleSquared);
+}
+
 void main() {
-    vec2 unitWorldX = vec2(1.0, 0.0);
-    vec2 unitClipX = mat2(transform) * unitWorldX;
-    float pixelsPerWorld = length(unitClipX * viewport_size * 0.5);
+    float pixelsPerWorld = weakestPixelsPerWorld();
     float minimumWorldThickness = minimum_feature_pixels / max(pixelsPerWorld, 0.000001);
     float effectiveThickness = max(thickness_instance, minimumWorldThickness);
     float maxRadius = max(radius_instance + effectiveThickness * 0.5, 0.0);
