4bc9fec53f8d56bf875f37348029ccec9f5cf6f5
dsafdsaf132 <dsafdsaf132@gmail.com>
2026-06-10 16:50:02 +0900

    Fix Bundlephobia wasm asset build

--- packages/wasm-gerber-renderer/shared.js
+++ packages/wasm-gerber-renderer/shared.js
@@ -1,6 +1,14 @@
 export const DEFAULT_WASM_MODULE_URLS = [
-  new URL("./wasm/wasm_gerber_processor.js", import.meta.url),
-  new URL("../../wasm/pkg/wasm_gerber_processor.js", import.meta.url),
+  // Bundle-size analyzers using webpack/Rspack reject auxiliary JS assets
+  // emitted from these package-relative WASM loader URLs.
+  new URL(
+    /* webpackIgnore: true */ "./wasm/wasm_gerber_processor.js",
+    import.meta.url,
+  ),
+  new URL(
+    /* webpackIgnore: true */ "../../wasm/pkg/wasm_gerber_processor.js",
+    import.meta.url,
+  ),
 ];
 
 export const DEFAULT_COLORS = [
